4b5e5bc7c33d141ef87e98227df40d81d857f727
David Peterman <peterman.dd@gmail.com>
2020-03-27 13:51:25 -0400

    added stress (mouse tracker) and countdown bars. Need to fix confetti (not too hard). Need to create some kind of event for when the timer runs out.

--- scourhour.js
+++ scourhour.js
@@ -39,8 +39,7 @@ let emojis = animalEmojis.concat(faceEmojis.concat(clothesEmojis.concat(foodEmoj
 let selectedEmojis = emojis;
 
 let progressBar = document.getElementById('progress');
-let progressBarStart = -5; //gets incremented with each time "waldo" is found
-let progressBarEnd = 0; //gets incremented with each time "waldo" is found   
+let progressBarProgress = 0;
 
 let numberOfEmojis = 10; //number of emojis displayed on screen at start. increases after every finding of waldo
 
@@ -50,12 +49,12 @@ let screenHeight = window.innerHeight;
 let waldoEmojiElement = document.getElementById('waldo-emoji');
 let allElements = document.querySelector('#emoji-class');
 let box = document.getElementById("box");
-let scoreFiller = document.getElementById("score-filler");
 
 document.body.style.transition = '1s';
 document.body.style.backgroundColor = bgChange();
 box.style.backgroundColor = 'lightyellow';
 
+let countdownTime = 20;
 
 ///////////////////////////////////////
 //Emoji Class to create Emoji instances
@@ -87,11 +86,14 @@ class Emoji {
 }
 
 
+///////////////////
+//Utility functions
+///////////////////
+
 //random number generator 
 function randomNum(number) {
     return Math.floor(Math.random() * number);
 }
-
 
 //get random color
 function bgChange() {
@@ -99,33 +101,12 @@ function bgChange() {
     return rndCol;
 }
 
-
 //return random emoji
 function getRandEmoji() {
     let emojiCell = randomNum(selectedEmojis.length);
     let emoji = selectedEmojis[emojiCell];
     return emoji;
 }
-
-
-//updates progress bar
-function setProgressBar() {
-    resetAnimation();
-    progressBarStart += 5;
-    progressBarEnd += 5;
-    progressBar.style.setProperty('--height0', progressBarStart + '%')
-    progressBar.style.setProperty('--height100', progressBarEnd + '%')
-
-}
-
-
-//resets progress bar animation
-function resetAnimation() {
-    progressBar.style.animation = 'none';
-    progressBar.offsetHeight; // trigger reflow 
-    progressBar.style.animation = null;
-}
-
 
 //releases confetti onto user screen
 function releaseConfetti() {
@@ -137,18 +118,6 @@ function releaseConfetti() {
     confetti.render();
 }
 
-
-//resets page after emoji is found
-function resetPage() {
-    box.style.backgroundColor = 'lightyellow';
-    document.body.style.backgroundColor = bgChange();    
-    while (document.getElementsByClassName('emoji-class')[0]) {
-        document.getElementsByClassName('emoji-class')[0].remove();
-    }
-    populatePage(); //repopulate the page with new emojis
-}
-
-
 //run when user has won the game
 function win() {
     releaseConfetti(); //user has "won", release confetti
@@ -156,6 +125,12 @@ function win() {
     audio.play(); //play trumpet win sound    
 }
 
+
+
+
+///////////////////
+//Setting up events
+///////////////////
 
 //create waldo elements and set event tiggers
 function setEmojiToFind(waldoEmoji) {
@@ -173,8 +148,12 @@ function setEmojiToFind(waldoEmoji) {
 
     //define onclick event when "waldo" is found
     elementToFind.onclick = function(e) {
-        setProgressBar();
-        if (progressBarEnd >= 100) { win(); return; }       
+        setProgressBar(); 
+        if(countdownTime > 5) {
+            countdownTime -= 5;        
+        }
+        setCountDownBar();
+        if (progressBar.style.width == '100%') { win(); return; }       
         //add found "waldo" emoji back into emojis array
         selectedEmojis.push(waldoEmoji.emoji);
         numberOfEmojis += 10; //increase difficulty
@@ -189,6 +168,26 @@ function setEmojiToFind(waldoEmoji) {
             resetPage();
         }, 600);
     }
+}
+
+
+
+
+///////////////////////////////////
+//Page reset and populate functions
+///////////////////////////////////
+
+
+//resets page after emoji is found
+function resetPage() {
+    box.style.backgroundColor = 'lightyellow';
+    document.body.style.backgroundColor = bgChange(); 
+       
+    while (document.getElementsByClassName('emoji-class')[0]) {
+        document.getElementsByClassName('emoji-class')[0].remove();
+    }
+    
+    populatePage(); //repopulate the page with new emojis
 }
 
 
@@ -216,38 +215,39 @@ function populatePage() {
 populatePage();
 
 
-
+////////////////////////////////
+//functions that set emoji types
+////////////////////////////////
 
 function setToAnimalEmojis() {
     selectedEmojis = animalEmojis;
     resetPage();
 }
-
 function setToFaceEmojis() {
     selectedEmojis = faceEmojis;
     resetPage();
 }
-
 function setToClothesEmojis() {
     selectedEmojis = clothesEmojis;
     resetPage();
 }
-
 function setToFoodEmojis() {
     selectedEmojis = foodEmojis;
     resetPage();
 }
-
 function setToTravelEmojis() {
     selectedEmojis = travelEmojis;
     resetPage();
 }
-
 function setToAllEmojis() {
     selectedEmojis = emojis;
     resetPage();
 }
 
+
+////////////////
+//Pull-Down Menu
+////////////////
 
 // Set the width of the side navigation to 26%
 function openNav() {    
@@ -256,13 +256,76 @@ function openNav() {
     document.getElementById("arrow").textContent = '⮭';
     document.getElementById("arrow").style.top = '78%';    
     document.getElementById("menu-btn").onclick = closeNav;  
-  }
+}
   
-  //Set the width of the side navigation to 5%
-  function closeNav() {        
+//Set the width of the side navigation to 5%
+function closeNav() {        
     document.getElementById("arrow").textContent = '⮯';
     document.getElementById("arrow").style.top = '20%';    
     document.getElementById('menu-btn').style.height = '100%';  
     document.getElementById('menu-btn').style.borderRadius = '0px 0px 6px 0px';    
     document.getElementById("menu-btn").onclick = openNav;                            
-  }
+}
+
+
+
+///////////////////////////
+//Progress/Stress/etc. Bars 
+///////////////////////////
+
+//resets progress animation
+function resetAnimation(subject) {
+    subject.style.animation = 'none';
+    subject.offsetHeight; // trigger reflow 
+    subject.style.animation = null;
+}
+
+
+//updates progress bar
+function setProgressBar() {    
+    progressBarProgress += 10;    
+    progressBar.style.setProperty('width', progressBarProgress + '%');  
+
+}
+
+
+//updates stress bar
+let prevStress;
+function setStressBar(stress) {
+    let stressBar = document.getElementById("stress")         
+    stressBar.style.setProperty('height', (stress*7) +"%");    
+    prevStress = stress;
+}
+
+
+function setCountDownBar() {     
+    let countdownBar = document.getElementById('countdown');     
+    resetAnimation(countdownBar);
+    countdownBar.style.setProperty('animation-duration',  countdownTime + 's' );
+           
+}
+
+
+////////////////
+//Mouse Tracking
+////////////////
+ 
+let x1, y1, x2, y2, stress;
+document.getElementById("box").onmousemove = function(event) {
+    mouseTracker(event)
+};
+
+function mouseTracker(e) {
+  
+    var x2 = e.clientX;
+    var y2 = e.clientY;   
+    
+    stress = Math.pow(Math.pow(x2-x1,2) + Math.pow(y2-y1,2),0.5) 
+    if(prevStress == null) {
+        prevStress = 0;
+    }
+    setStressBar(stress);           
+        
+    x1 = x2
+    y1 = y2       
+}
